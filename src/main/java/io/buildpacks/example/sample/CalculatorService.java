ee5243ef40d0a8738c229c2807290eb215763402
venkatamohit <58447812+venkatamohit@users.noreply.github.com>
2024-06-16 18:22:50 -0400

    Update CalculatorService.java

--- src/main/java/io/buildpacks/example/sample/CalculatorService.java
+++ src/main/java/io/buildpacks/example/sample/CalculatorService.java
@@ -2,25 +2,63 @@ package io.buildpacks.example.sample;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Service class providing basic calculator operations.
+ */
 @Service
 public class CalculatorService {
 
-    public int add(int a, int b) {
+    /**
+     * Performs addition of two integers.
+     *
+     * @param a The first integer.
+     * @param b The second integer.
+     * @return The sum of a and b.
+     */
+    public int performAddition(int a, int b) {
+        // TODO: Add input parameter validation here if necessary
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    /**
+     * Performs subtraction of two integers.
+     *
+     * @param a The first integer (minuend).
+     * @param b The second integer (subtrahend).
+     * @return The result of subtracting b from a.
+     */
+    public int performSubtraction(int a, int b) {
+        // TODO: Add input parameter validation here if necessary
         return a - b;
     }
 
-    public int multiply(int a, int b) {
+    /**
+     * Performs multiplication of two integers.
+     *
+     * @param a The first integer.
+     * @param b The second integer.
+     * @return The product of a and b.
+     */
+    public int performMultiplication(int a, int b) {
+        // TODO: Add input parameter validation here if necessary
         return a * b;
     }
 
-    public int divide(int a, int b) throws IllegalArgumentException {
+    /**
+     * Performs division of two integers.
+     *
+     * @param a The numerator.
+     * @param b The denominator.
+     * @return The result of dividing a by b.
+     * @throws IllegalArgumentException If b is zero.
+     */
+    public int performDivision(int a, int b) throws IllegalArgumentException {
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed.");
         }
         return a / b;
     }
+
+    // Other methods can be similarly refactored and documented as needed
+
 }
