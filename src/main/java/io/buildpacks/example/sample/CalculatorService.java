7fc85f130a9bc34a9bfa5d1beddc6ef0d28d7b78
venkatamohit <58447812+venkatamohit@users.noreply.github.com>
2024-06-16 17:07:19 -0400

    Update CalculatorService.java

--- src/main/java/io/buildpacks/example/sample/CalculatorService.java
+++ src/main/java/io/buildpacks/example/sample/CalculatorService.java
@@ -23,35 +23,4 @@ public class CalculatorService {
         }
         return a / b;
     }
-
-    // Removed intentional errors
-
-    // Unused private method removed
-    // private void unusedPrivateMethod() {
-    //     System.out.println("This method is never used.");
-    // }
-
-    // Unused parameter removed from subtract method
-    // public int subtract(int a, int b, int c) {
-    //     return a - b;
-    // }
-
-    // Removed misleading sum method
-    // public int sum(int a, int b) {
-    //     return a + b;
-    // }
-
-    // Corrected divide method to throw IllegalArgumentException
-    // public int divide(int a, int b) {
-    //     try {
-    //         return a / b;
-    //     } catch (ArithmeticException e) {
-    //         return 0;
-    //     }
-    // }
-
-    // Renamed Subtract method to follow naming convention
-    // public int Subtract(int a, int b) {
-    //     return a - b;
-    // }
 }
