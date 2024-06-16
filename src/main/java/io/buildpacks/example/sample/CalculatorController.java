6e9b12b8addfa69fe5d3e6c594927eb6facf2b24
venkatamohit <58447812+venkatamohit@users.noreply.github.com>
2024-06-16 10:34:20 -0400

    Create CalculatorController.java

---
+++ src/main/java/io/buildpacks/example/sample/CalculatorController.java
@@ -0,0 +1,33 @@
+package io.buildpacks.example.sample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+
+    @Autowired
+    private CalculatorService calculatorService;
+
+    @GetMapping("/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.add(a, b);
+    }
+
+    @GetMapping("/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.subtract(a, b);
+    }
+
+    @GetMapping("/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.multiply(a, b);
+    }
+
+    @GetMapping("/divide")
+    public int divide(@RequestParam int a, @RequestParam int b) {
+        return calculatorService.divide(a, b);
+    }
+}
